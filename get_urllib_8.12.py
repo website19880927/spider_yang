938b5c9d9bb97ad640102a0dfac092a36934eb6d
website19880927 <659706575@qq.com>
2018-08-12 13:27:21 +0800

    create urllib

---
+++ get_urllib_8.12.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: caopeng
+@license: (C) Copyright 2018-2019 @yang.com Corporation Limited.
+@contact: 659706575@qq.com
+@software: garner
+@file: get_urllib_8.12.py
+@time: 2018/8/12 0012 13:18
+@desc:url发送请求
+'''
+from urllib import request as req
+
+req.urlopen(url='http://www.baidu.com',timeout=2)
+
+
+
+
+
